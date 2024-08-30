1305a53a0b8dfcc887b27ca718982e7424ce7168
Muhammad Bilal <muhmdbilal3333@gmail.com>
2024-08-31 00:41:39 +0500

    nft schema create

--- routes/nftMintingRoute.js
+++ routes/nftMintingRoute.js
@@ -11,5 +11,13 @@ router.post("/create-nft-minting", async (req, res) => {
       res.status(500).json({ message: 'Server Error' });
     }
 })
+router.get("/get-nft-minting/:id", async (req, res) => {
+  try{
+     const response  = await NftMinting.find({userId: req.params.id});
+     res.status(201).json({ message: 'Nft Minting Get Successfully', response });
+  }catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+})
 
 module.exports = router;
